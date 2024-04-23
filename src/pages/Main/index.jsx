53456714947d84963a3e89709293553d63bcb91c
nonato3009 <nonato3@hotmail.com>
2024-04-22 22:15:22 -0300

    API GitHub filtrando os repositórios

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { MdSearch } from "react-icons/md";
 
-import { Container } from "./styles";
+import { Container, Logo, Title, Form, Input, Button } from "./styles";
+
+import githubLogo from "../../assets/images/github-logo.svg"
 
 export default function Main() {
+  const [login, setLogin] = useState('');
   return (
     <Container>
-      <img
-        src="https://devsamurai-materials.s3.amazonaws.com/templates/avatar-gradient-dark.svg"
-        height="256"
-        alt="Dev Samurai"
+      <Logo
+        src={ githubLogo }
+        alt="API Github"
       />
-      <h1>Fala Samurai!</h1>
-      <p>Esse é o template typescript básico da Dev Samurai para React.</p>
+      <Title>API Github</Title>
+      <Form>
+        <Input placeholder="usuário" value={login} onChange={(event) => setLogin(event.target.value)} />
+        <Button to={`/${login}/repositories`}>
+          <MdSearch size={42} color="white" />
+        </Button>
+      </Form>
+      <p>Template Github criado por Nonatu`s Coder.</p>
     </Container>
   );
 }
--- src/routes.jsx
+++ src/routes.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import MainPage from "./pages/Main";
+import RepositoriesPage from "./pages/RepositoriesPage";
 
 export function AppRoutes() {
   return (
-    <Routes>
-      <Route path="/" element={<MainPage />} />
-    </Routes>
+    <><Routes>
+        <Route path="/:login/repositories" element={<RepositoriesPage />} />
+      </Routes>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </>
   );
 }
